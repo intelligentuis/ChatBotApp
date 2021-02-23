1fc6a159d9fa89b3fe515d49f2c47a2d6d52d356
aaiit <abdellatif.aithammadi@gmail.com>
2021-02-23 12:22:01 +0100

    ++++

--- app/src/main/java/com/example/upendra/myapplication/Util/Admob.java
+++ app/src/main/java/com/example/upendra/myapplication/Util/Admob.java
@@ -20,7 +20,7 @@ public class Admob {
     {
 
         mInterstitial = new InterstitialAd(context);
-        mInterstitial.setAdUnitId("ca-app-pub-6614596976194381~6139882229");
+        mInterstitial.setAdUnitId("ca-app-pub-6614596976194381/9647882393");
         mInterstitial.setAdListener(new AdListener() {
             @Override
             public void onAdLoaded() {
@@ -67,7 +67,7 @@ public class Admob {
     public static void loadInterstitial() {
 
         mInterstitial.loadAd(new AdRequest.Builder().
-                addTestDevice("ca-app-pub-6614596976194381~6139882229").//ca-app-pub-3940256099942544/1033173712
+                addTestDevice("ca-app-pub-6614596976194381/9647882393").//ca-app-pub-3940256099942544/1033173712
                 build());
     }
 
@@ -80,7 +80,7 @@ public class Admob {
     public static void createLoadBanner(final Context context, View view) {
         mAdView = (AdView) view.findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().
-                addTestDevice("ca-app-pub-6614596976194381~6139882229                                                                                                                                                                                                                                                                                                                                                                                                                                       ").//ca-app-pub-6614596976194381~6139882229
+                addTestDevice("ca-app-pub-6614596976194381/9647882393                                                                                                                                                                                                                                                                                                                                                                                                                                       ").//ca-app-pub-6614596976194381/9647882393
                 build();
         mAdView.loadAd(adRequest);
 
